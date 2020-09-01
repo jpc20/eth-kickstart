619643b7c88c60b1ca6beb939de5c559d04e7aaa
Jack Cullen <jpcullen20@gmail.com>
2020-09-01 15:54:32 -0600

    Update header

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default () => {
   return (
     <Menu style={{ marginTop: "10px" }}>
       <Link route="/">
-        <a className="item">CrowdCoin</a>
+        <a className="item">Eth-Kickstart</a>
       </Link>
 
       <Menu.Menu position="right">
